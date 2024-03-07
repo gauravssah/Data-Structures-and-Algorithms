1047adb01c148b14e94f6e42dc94e62ae09e9b57
gauravssah <124705806+gauravssah@users.noreply.github.com>
2024-03-07 15:19:11 +0530

    mearge sort

--- 01 APNA COLLEGE/05ArraysInCpp/12MergeSort.cpp
+++ 01 APNA COLLEGE/05ArraysInCpp/12MergeSort.cpp
@@ -1,3 +1,5 @@
+// Merge Sort is a stable sort which means that the same element in an array maintain their original positions with respect to each other. Overall time complexity of Merge sort is O(nLogn). It is more efficient as it is in worst case also the runtime is O(nlogn)
+
 #include <iostream>
 using namespace std;
 
