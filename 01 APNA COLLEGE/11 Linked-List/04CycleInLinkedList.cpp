07dc3f5b5b5700a9ae2be0b52af70e83f5c155bb
gauravssah <124705806+gauravssah@users.noreply.github.com>
2024-03-15 09:33:39 +0530

    cycle in linkedlist

---
+++ 01 APNA COLLEGE/11 Linked-List/04CycleInLinkedList.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtTail(node *&head, int val)
+{
+    node *newNode = new node(val);
+
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+};
+
+void makeCycle(node *&head, int pos)
+{
+    node *temp = head;
+    node *startNode;
+
+    int count = 1;
+    while (temp->next != NULL)
+    {
+        if (count == pos)
+        {
+            startNode = temp;
+        }
+        temp = temp->next;
+        count++;
+    }
+
+    temp->next = startNode;
+};
+
+bool detectCycle(node *&head)
+{
+    node *slow = head;
+    node *fast = head;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (fast == slow)
+        {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+void removeCycle(node *&head)
+{
+    node *slow = head;
+    node *fast = head;
+
+    do
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    } while (slow != fast);
+
+    fast = head;
+    while (slow->next != fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    slow->next = NULL;
+};
+
+void displayNodes(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL";
+};
+
+main()
+{
+    node *head = NULL;
+
+    insertAtTail(head, 10);
+    insertAtTail(head, 20);
+    insertAtTail(head, 30);
+    insertAtTail(head, 40);
+    insertAtTail(head, 50);
+    insertAtTail(head, 60);
+    insertAtTail(head, 70);
+    insertAtTail(head, 80);
+    displayNodes(head);
+    cout << endl;
+
+    makeCycle(head, 3);
+    // displayNodes(head);
+    // cout << endl;
+
+    cout << detectCycle(head) << endl;
+    removeCycle(head);
+    cout << detectCycle(head) << endl;
+    // displayNodes(head);
+    return 0;
+}
