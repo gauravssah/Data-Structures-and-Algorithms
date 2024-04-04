dd176963aef040f64a257e1fc190f7d5d997ba40
gauravssah <124705806+gauravssah@users.noreply.github.com>
2024-04-04 07:27:33 +0530

    reverse the given sentence

---
+++ 01 APNA COLLEGE/12 Stack/02ReverseSentence.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+#include <stack>
+
+void reverseSentence(string s)
+{
+    stack<string> st;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+
+        string word = "";
+        while (s[i] != ' ' && i < s.length())
+        {
+            word += s[i];
+            i++;
+        }
+
+        st.push(word);
+    }
+
+    while (!st.empty())
+    {
+        cout << st.top() << " ";
+        st.pop();
+    }
+    cout << endl;
+}
+
+int main()
+{
+
+    string s = "Hey, how Are Doing?";
+    reverseSentence(s);
+
+    return 0;
+}
